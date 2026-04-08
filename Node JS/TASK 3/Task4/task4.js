b379298652ae8ccd0733166ac42c1e69bb91f7e0
VenilaVarshini200662 <varshinivenila@gmail.com>
2026-04-08 21:13:32 +0530

    Create task4.js

---
+++ Node JS/TASK 3/Task4/task4.js
@@ -0,0 +1,31 @@
+const http= require('http');
+const fs= require('fs');
+const server = http.createServer((req,res) => {
+    if(req.url ==='/readfile'){
+        fs.readFile('content.txt','utf8',(err, data) => {
+            if(err){
+                res.writeHead(500);
+                res.end('Error reading file');
+                return;
+            }
+            res.writeHead(200,{'Content-Type':'text/plain'});
+            res.end(data); 
+        });
+    }
+    else if(req.url ==='/streamfile'){
+        const stream = fs.createReadStream('content.txt','utf8');
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        stream.pipe(res); 
+        stream.on('error',(err) => {
+            res.writeHead(500);
+            res.end('Error reading file');
+        });
+    }
+    else{
+        res.writeHead(404);
+        res.end('Page not found');
+    }
+});
+server.listen(3000,() => {
+    console.log('Server running at http://localhost:3000');
+});
