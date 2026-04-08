d65a412898d4fd00e4c7565f6831ebd237a20f94
VenilaVarshini200662 <varshinivenila@gmail.com>
2026-04-08 22:00:53 +0530

    Create task12.js

---
+++ Node JS/TASK 3/Task12/task12.js
@@ -0,0 +1,11 @@
+const EventEmitter= require('events');
+const eventEmitter= new EventEmitter();
+eventEmitter.once('serverStart',() => {
+    console.log('Server started for the first time');
+});
+eventEmitter.on('serverStart',() => {
+    console.log('Server is running');
+});
+eventEmitter.emit('serverStart');
+eventEmitter.emit('serverStart');
+eventEmitter.emit('serverStart');
