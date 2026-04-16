fcfac297af559b098d1ea6cdb85b6dc9c816f4f3
VenilaVarshini200662 <varshinivenila@gmail.com>
2026-04-16 14:53:08 +0530

    Create task1.js

---
+++ Node JS/TASK 4/Task1/task1.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+app.get('/' ,(req,res)=>{
+    res.json({
+        message: "Welcome to learn Express"
+    });
+});
+
+app.get('/status' , (req,res)=>{
+    res.json({
+        message:"Server is running",
+        status:"OK"
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
