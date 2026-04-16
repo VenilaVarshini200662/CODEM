dc1b5308286ac78026287bbb5cf51e1075ae9814
VenilaVarshini200662 <varshinivenila@gmail.com>
2026-04-16 15:03:23 +0530

    Delete Node JS/TASK 4/task4 directory

--- Node JS/TASK 4/task4/public/script.js
+++
@@ -1,9 +0,0 @@
-function makeChanges(){
-    const imgDiv = document.getElementById("imgContainer");
-    const button = document.getElementById("btn");
-    imgDiv.innerHTML = '<img src="Exp.jpg"></img>';
-    button.style.backgroundColor="darkgreen";
-    button.innerText="Button clicked!";
-    button.style.color="whitesmoke";
-
-}
